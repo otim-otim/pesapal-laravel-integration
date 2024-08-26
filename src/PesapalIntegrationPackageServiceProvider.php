2e27af8cfb7caf4e8dcd2d4039a47406dc538cd4
Otim Isaac <isaacomega16@gmail.com>
2024-08-26 15:27:16 +0300

    add service provider class

---
+++ src/PesapalIntegrationPackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OtimOtim\PesapalIntegrationPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class PesapalIntegrationPackageServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    //
+  }
+
+  public function boot()
+  {
+    //
+  }
+}
