62500e14675d3cfc341c02d3e56d06eae9057732
Otim Isaac <isaacomega16@gmail.com>
2024-08-27 16:32:42 +0300

    rename transaction model name to match convention

--- src/Models/PesapalTransaction.php
+++ src/Models/PesapalTransaction.php
@@ -4,12 +4,21 @@ namespace OtimOtim\PesapalIntegrationPackage\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class PesapalTransactions extends Model
+class PesapalTransaction extends Model
 {
   use HasFactory;
 
   // Disable Laravel's mass assignment protection
   protected $guarded = []; 
   
+
+  public function usable(): MorphTo{
+    return $this->morphTo();
+}
+
+public function modelable(): MorphTo{
+    return $this->morphTo();
+}
 }
