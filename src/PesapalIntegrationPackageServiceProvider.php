af6771b4a53e33ead7966d33803bd3624dfb8a81
Otim Isaac <isaacomega16@gmail.com>
2024-09-07 02:09:06 +0300

    check for complete configuration from service provider

--- src/PesapalIntegrationPackageServiceProvider.php
+++ src/PesapalIntegrationPackageServiceProvider.php
@@ -23,5 +23,40 @@ class PesapalIntegrationPackageServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     
       }
+
+      $this->ensureConfigIsSet();
   }
+
+  /**
+     * Ensure all required config variables are set.
+     *
+     * @throws \Exception
+     */
+    protected function ensureConfigIsSet()
+    {
+        $requiredConfigKeys = [
+            'PesapalIntegrationPackage.CONSUMER_KEY',
+            'PesapalIntegrationPackage.CONSUMER_SECRET',
+            'PesapalIntegrationPackage.NOTIFICATION_ID',
+            'PesapalIntegrationPackage.NOTIFICATION_URL',
+            'PesapalIntegrationPackage.CALLBACK_URL',
+        ];
+
+        // Check environment-specific URL
+        $environment = config('APP_ENV');
+        if ($environment == 'production') {
+            $requiredConfigKeys[] = 'PesapalIntegrationPackage.LIVE_URL';
+        } else {
+            $requiredConfigKeys[] = 'PesapalIntegrationPackage.SAND_BOX_URL';
+        }
+
+
+
+
+        foreach ($requiredConfigKeys as $key) {
+            if (!config($key)) {
+                throw new \Exception("Pesapal Integration Package is not configured. Missing: {$key}");
+            }
+        }
+    }
 }
